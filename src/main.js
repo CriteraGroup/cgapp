34667de991a1f53959c974bd21dbe7d61d995f5d
Kevin Warnock <kevinwarnock@Kevins-MacBook-Pro.local>
2016-09-10 14:48:04 -0400

    folterung(enhancement): Add additional fields

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ const {CGApp} = require('./home');
 const {loadXLSX} = require('./home/load.js');
 const Excel = require('exceljs');
 
-let fs = require('fs');
+const DOMAIN = 'domain';
+const OBJECTIVE = 'objective';
+const SOURCE = 'source';
+const REASON = 'reason';
+const QUESTION = 'question';
+const CUSTOMER_RESPONSE = 'customer response';
+const AUDITOR_NOTES = 'auditor notes';
+const ANSWER = 'answer';
+const POLICY_DEFINED = 'policy defined';
+const CONTROL_IMPLEMENTED = 'control implemented';
+const CONTROL_AUTOMATED = 'control automated or technically enforced';
+const CONTROL_REPORTED = 'control reported to business';
+const STATUS = 'status';
 
+let fs = require('fs');
 let ipc = require('electron').ipcMain;
 
 ipc.on('bodyLoaded', function(event) {
@@ -17,36 +30,39 @@ ipc.on('save', function(event, rows) {
   var sheet,
     workbook = new Excel.Workbook();
 
-  workbook.creator = 'KWarnock';
-  workbook.lastModifiedBy = 'Them';
-  workbook.created = new Date(1985, 8, 30);
-  workbook.modified = new Date();
-
   sheet = workbook.addWorksheet('Dataset');
   
   sheet.columns = [
-    { header: 'domain', key: 'domain', width: 10 },
-    { header: 'objective', key: 'objective', width: 10 },
-    { header: 'policy derived', key: 'policy derived', width: 10 },
-    { header: 'reason', key: 'reason', width: 10 },
-    { header: 'question', key: 'question', width: 10 },
-    { header: 'customer response', key: 'customer response', width: 10 },
-    { header: 'auditor notes', key: 'auditor notes', width: 10 },
-    { header: 'policy defined', key: 'policy defined', width: 10 },
-    { header: 'control implemented', key: 'control implemented', width: 10 },
-    { header: 'control automated or technically enforced', key: 'control automated or technically enforced', width: 10 },
-    { header: 'control reported to business', key: 'control reported to business', width: 10 },
-    { header: 'status', key: 'status', width: 10 }
+    { header: DOMAIN, key: DOMAIN, width: 10 },
+    { header: OBJECTIVE, key: OBJECTIVE, width: 10 },
+    { header: SOURCE, key: SOURCE, width: 10 },
+    { header: REASON, key: REASON, width: 10 },
+    { header: QUESTION, key: QUESTION, width: 10 },
+    { header: CUSTOMER_RESPONSE, key: CUSTOMER_RESPONSE, width: 10 },
+    { header: AUDITOR_NOTES, key: AUDITOR_NOTES, width: 10 },
+    { header: ANSWER, key: ANSWER, width: 10 },
+    { header: POLICY_DEFINED, key: POLICY_DEFINED, width: 10 },
+    { header: CONTROL_IMPLEMENTED, key: CONTROL_IMPLEMENTED, width: 10 },
+    { header: CONTROL_AUTOMATED, key: CONTROL_AUTOMATED, width: 10 },
+    { header: CONTROL_REPORTED, key: CONTROL_REPORTED, width: 10 },
+    { header: STATUS, key: STATUS, width: 10 }
   ];
 
   rows.forEach(function loopRows(row) {
       sheet.addRow({
-        'domain': row.domain,
-        'objective': row.section,
-        'question': row.question,
-        'customer response': row['customer response'],
-        'auditor notes': row['auditor notes'],
-        'status': typeof row.status !== "undefined" ? row.status.text : null
+        'domain': row[DOMAIN],
+        'objective': row[OBJECTIVE],
+        'source': row[SOURCE],
+        'reason': row[REASON],
+        'question': row[QUESTION],
+        'customer response': row[CUSTOMER_RESPONSE],
+        'auditor notes': row[AUDITOR_NOTES],
+        'answer': row[ANSWER],
+        'policy defined': typeof row[POLICY_DEFINED] !== 'undefined' ? row[POLICY_DEFINED].text : null,
+        'control implemented': row[CONTROL_IMPLEMENTED],
+        'control automated or technically enforced': row[CONTROL_AUTOMATED],
+        'control reported to business': row[CONTROL_REPORTED],
+        'status': typeof row[STATUS] !== 'undefined' ? row[STATUS].text : null 
       });
   });
 
